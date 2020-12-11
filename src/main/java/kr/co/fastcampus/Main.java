173980ddf2e40b7ae87785d58e85a7acd55b94b1
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-12-11 17:53:21 +0900

    feat : Initialize pom.xml, .gitignore

---
+++ src/main/java/kr/co/fastcampus/Main.java
@@ -0,0 +1,12 @@
+package kr.co.fastcampus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world!!");
+    }
+}
