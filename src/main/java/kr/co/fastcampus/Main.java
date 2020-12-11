157207fa16d46f5bcdf60e9202c9e51f54eedc12
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-12-11 19:04:04 +0900

    test : Refactor JDBC test

--- src/main/java/kr/co/fastcampus/Main.java
+++ src/main/java/kr/co/fastcampus/Main.java
@@ -8,56 +8,30 @@ import java.sql.*;
 public class Main {
     private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         logger.info("Hello world!!");
 
-        Connection connection = null;
-        Statement statement = null;
+        Class.forName("org.h2.Driver");
+        var url = "jdbc:h2:mem:test;MODE=MySQL;";
 
-        try {
-            Class.forName("org.h2.Driver");
-            String url = "jdbc:h2:mem:test;MODE=MySQL;";
-            connection = DriverManager.getConnection(url, "sa", "");
-            statement = connection.createStatement();
+        try (var connection = DriverManager.getConnection(url, "sa", "");
+            var statement = connection.createStatement())   {
 
             connection.setAutoCommit(false);
-
             statement.execute("create table member(id int auto_increment, username varchar(255) not null, password varchar(255) not null, primary key(id))");
-
-            statement.executeUpdate("insert into member(username, password) values('minseong', '1234')");
-
-
-
-            ResultSet resultSet = statement.executeQuery("select id, username, password from member");
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String username = resultSet.getString("username");
-                String password = resultSet.getString("password");
-
-                logger.info("id: " + id + " username: " + username + " password: " + password);
-            }
-            
-            connection.commit();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
             try {
+                statement.executeUpdate("insert into member(username, password) values('minseong', '1234')");
+                connection.commit();
+            } catch (SQLException e) {
                 connection.rollback();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
             }
-        } finally {
-            try {
-                statement.close();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
+
+            var resultSet = statement.executeQuery("select id, username, password from member");
+            while (resultSet.next()) {
+                Member member = new Member(resultSet);
+                logger.info(member.toString());
             }
-            try {
-                connection.close();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
+        } catch (SQLException e) {
         }
     }
 }
